b587b67d19c1a1b614df01800ad8435e0e2c3c36
Administrator <administrator@email.ru>
2024-08-06 18:32:30 +0300

    commit

--- src/client/java/net/gurudev/storytelling/MainClient.java
+++ src/client/java/net/gurudev/storytelling/MainClient.java
@@ -2,13 +2,16 @@ package net.gurudev.storytelling;
 
 import net.fabricmc.api.ClientModInitializer;
 import net.gurudev.storytelling.entity.EntityModelManager;
+import net.gurudev.storytelling.gui.ScreenHandlerManager;
+import net.gurudev.storytelling.gui.StorytellerScreen;
 import net.gurudev.storytelling.input.KeybindingHandler;
+import net.minecraft.client.gui.screen.ingame.HandledScreens;
 
 import static net.gurudev.storytelling.Main.LOGGER;
 
 public class MainClient implements ClientModInitializer {
 	@Override public void onInitializeClient() {
-
+		HandledScreens.register(ScreenHandlerManager.STORYTELLER_SCREEN_HANDLER_TYPE, StorytellerScreen::new);
 		LOGGER.info("Registering keybindings..."); KeybindingHandler.registerKeybindings();
 		EntityModelManager.registerEntities();
 	}
--- src/client/java/net/gurudev/storytelling/gui/StorytellerScreen.java
+++ src/client/java/net/gurudev/storytelling/gui/StorytellerScreen.java
@@ -4,44 +4,50 @@ import net.gurudev.storytelling.entity.StorytellerEntity;
 import net.gurudev.storytelling.gui.widget.ButtonRowElement;
 import net.gurudev.storytelling.gui.widget.GuiElement;
 import net.gurudev.storytelling.gui.widget.TextRowElement;
-import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.DrawContext;
-import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.gui.screen.ingame.HandledScreen;
 import net.minecraft.client.gui.widget.ButtonWidget;
 import net.minecraft.client.gui.widget.TextWidget;
-import net.minecraft.nbt.NbtCompound;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.text.Text;
 
-public class StorytellerScreen extends Screen {
-    private final StorytellerEntity entity;
+import java.util.Optional;
 
-    public StorytellerScreen(StorytellerEntity entity) {
-        super(Text.empty()); this.entity = entity;
+public class StorytellerScreen extends HandledScreen<StorytellerScreenHandler> {
+    private final PlayerEntity player;
+    public StorytellerScreen(StorytellerScreenHandler handler, PlayerInventory inventory, Text text) {
+        super(handler, inventory, Text.empty());
+        this.player = inventory.player;
+    }
+
+    protected Optional<StorytellerEntity> getStorytellerEntity() {
+        return Optional.of(this.handler).flatMap(StorytellerScreenHandler::getStorytellerEntity);
     }
 
     @Override
     protected void init() {
         super.init(); GuiElement gui = new GuiElement(width, height);
 
-        TextRowElement textureRow = new TextRowElement(this, this.textRenderer, -gui.offset*2, Text.translatable("storytelling.settings.texture"), entity.getTexture());
+        TextRowElement textureRow = new TextRowElement(this, this.textRenderer, -gui.offset*2, Text.translatable("storytelling.settings.texture"), "");
         this.addDrawableChild(textureRow.getText()); this.addDrawableChild(textureRow.getElement());
 
-        TextRowElement modelRow = new TextRowElement(this, this.textRenderer, -gui.offset, Text.translatable("storytelling.settings.model"), entity.getModel());
+        TextRowElement modelRow = new TextRowElement(this, this.textRenderer, -gui.offset, Text.translatable("storytelling.settings.model"), "");
         this.addDrawableChild(modelRow.getText()); this.addDrawableChild(modelRow.getElement());
 
         ButtonRowElement storylineRow = new ButtonRowElement(this, this.textRenderer, gui.offset, Text.translatable("storytelling.settings.storyline"), callback ->
-                MinecraftClient.getInstance().setScreen(new StorylineScreen(entity)));
+                this.close());
         this.addDrawableChild(storylineRow.getText()); this.addDrawableChild(storylineRow.getElement());
 
         TextWidget titleText = new TextWidget(gui.centerX-gui.guiWidth/2, gui.centerY/2-gui.guiHeight/2, gui.guiWidth, gui.guiHeight,
                 Text.translatable("storytelling.settings.title"), this.textRenderer);
 
         ButtonWidget successButton = ButtonWidget.builder(Text.translatable("gui.done"), callback -> {
-            NbtCompound nbtCompound = new NbtCompound();
-            nbtCompound.putString("texture", textureRow.getElement().getText());
-            nbtCompound.putString("model", modelRow.getElement().getText());
-            entity.readCustomDataFromNbt(nbtCompound);
-            MinecraftClient.getInstance().setScreen(null);
+//            NbtCompound nbtCompound = new NbtCompound();
+//            nbtCompound.putString("texture", textureRow.getElement().getText());
+//            nbtCompound.putString("model", modelRow.getElement().getText());
+//            entity.readCustomDataFromNbt(nbtCompound);
+            this.close();
         }).dimensions(gui.centerX-gui.guiWidth/2, gui.centerY+gui.centerY/2-gui.guiHeight/2, gui.guiWidth, gui.guiHeight).build();
         this.addDrawableChild(titleText); this.addDrawableChild(successButton);
     }
@@ -52,6 +58,8 @@ public class StorytellerScreen extends Screen {
     public void render(DrawContext context, int mouseX, int mouseY, float delta) {
         super.render(context, mouseX, mouseY, delta);
     }
+
+    @Override protected void drawBackground(DrawContext context, float delta, int mouseX, int mouseY) {}
 
 //    drawableElement.setX(drawableElement.getX()-drawableElement.getWidth()/2);
 //    drawableElement.setY(drawableElement.getX()-drawableElement.getHeight()/2);
